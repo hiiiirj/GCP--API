df4ee24b0feabfc8b06297ad42548d0e1e745a78
rajat-srivastava <52487806+rajat11299@users.noreply.github.com>
2024-04-22 12:54:33 +0530

    Update main.py

--- Statefile-upload/main.py
+++ Statefile-upload/main.py
@@ -26,7 +26,7 @@ def upload_file_to_gcs(file_path, bucket_name):
 
 if __name__ == "__main__":
     # Set your file path and bucket name
-    file_path = os.environ['file-path']
+    file_path = os.environ['INPUT_STORE']
     bucket_name = "my-bucket3722"
 
     # Upload the file and get the URL
